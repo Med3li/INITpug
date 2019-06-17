6fd2b35eb2c80da4d760b1bac817361c006cb48d
mohamed ali <meda9194@gmail.com>
2019-06-17 01:06:24 +0100

    global update

--- gulpfile.js
+++ gulpfile.js
@@ -17,13 +17,22 @@ return gulp.src(['css/*.css', '!css/bootstrap- v3.3.7.css'])
   .pipe(notify("polyfill task is done!"));
 });
 //my 6th task
-gulp.task('copy', function(){
+gulp.task('css-copy', function(){
    // require('./server.js');
    return gulp.src(['css/bootstrap- v3.3.7.css'])//extension = js/css/pug
      .pipe(gulp.dest('dist/css'))
-     .pipe(notify("copying files task is done!"));
+     .pipe(notify("css-copy task is done!"));
 });
-//*********my 7h task
+//my 6th task
+gulp.task('js-copy', function(){
+   // require('./server.js');
+   return gulp.src(['js/*.js'])//extension = js/css/pug
+     .pipe(gulp.dest('dist/js'))
+     .pipe(notify("js-copy task is done!"));
+});
+//*********my 11th task
+//gulp.task('ready', ['copy-css', 'copy-js', 'create-polyfill-file']);
+//*********my 7th task
 gulp.task('sasstocss',function() {
 
 //require('./server.js');
@@ -51,8 +60,8 @@ return gulp.src('pug js/*.pug')
 gulp.task('jsminify',function() {
 
 //require('./server.js');
-return gulp.src('dist/js/main.js')
-        .pipe(concat('main.js'))
+return gulp.src(['js/main.js'])
+        .pipe(concat('main.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist/js'))  //destribution
         .pipe(notify("JS minify is done !"))
@@ -65,8 +74,9 @@ gulp.task('watch', function(){
       gulp.watch('js/*.js', gulp.series('jsminify'))
       gulp.watch('pug js/*.pug', gulp.series('pugtohtml'))
       gulp.watch(['css/*.css', '!css/bootstrap- v3.3.7.css'], gulp.series('create-polyfill-file'))
-      gulp.watch(['css/bootstrap- v3.3.7.css'], gulp.series('copy'))
-      gulp.watch(['sass/*.scss'], gulp.series('sasstocss'));
+      gulp.watch('css/bootstrap- v3.3.7.css', gulp.series('css-copy'))
+      gulp.watch('js/*.js', gulp.series('js-copy'))
+      gulp.watch('sass/*.scss', gulp.series('sasstocss'));
 });
 //Default task
 gulp.task('default', gulp.parallel('watch')); //gulp.series was here!
