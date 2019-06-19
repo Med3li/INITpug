4a2b5d9cbaf2627b77876baa4afb400b3e826941
mohamed ali <meda9194@gmail.com>
2019-06-19 15:46:37 +0100

    Modify the output of server.js on command line

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var server       = new StaticServer({
   rootPath: 'dist',            // required, the root of the server file tree
   port: 7200                  // required, the port to listen 
 });
-
 server.start( function () {
 console.log('Server listening to', server.port + ', This starts server on http://localhost:' + server.port + '/...'); //x,x => x x | x+x => xx
 });
